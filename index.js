8f726ab9066f1c9da4c953ef76ef680992a506d6
Expressionlessliew <xinyi.liew09@gmail.com>
2023-05-25 21:29:54 +0800

    hello

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var myVar;
-    
+
 function Loader() {
-  myVar = setTimeout(showPage, 50000);
+  myVar = setTimeout(showPage, 1500);
 }
 
 function showPage() {
@@ -11,4 +11,18 @@ function showPage() {
   document.getElementById("loader4").style.display = "none";
   document.getElementById("loader5").style.display = "none";
   document.getElementById("loader6").style.display = "none";
-}    
+
+  // Hide elements with the class name "glowing-text"
+  var glowingTextElements = document.querySelectorAll(".glowing-text");
+  for (var i = 0; i < glowingTextElements.length; i++) {
+    glowingTextElements[i].style.display = "none";
+  }
+
+  document.getElementById("mainContent").style.display = "block";
+  document.getElementById("L8Y").style.display = "block"; // Show the #L8Y element
+  document.getElementById("ABOUT").style.display = "block"; // Show the #L8Y element
+  document.getElementById("gmail").style.display = "block"; // Show the #L8Y element
+  document.getElementById("time").style.display = "block"; // Show the #L8Y element
+  document.getElementById("images").style.display = "block"; // Show the #L8Y element
+
+}
