23a2a02b03dd7ee706c136cf65325ae1982321e4
Expressionlessliew <xinyi.liew09@gmail.com>
2024-02-29 18:16:40 +0800

    check20

--- work/work.js
+++ work/work.js
@@ -50,3 +50,10 @@ function time() {
   }
   setInterval(time, 10);
   
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
